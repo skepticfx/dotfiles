0bdf51e61ae2f9edc18d2267e555083d4b11fe27
skepticfx <ahamed.nafeez@gmail.com>
2025-10-29 11:41:28 -0700

    update to neovim 0.11 with builtin lsp and other goodies

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,17 @@
+-- Disable netrw completely
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- remap leader to space
+vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>i", "miHmtgg=G'ti", { noremap = true, silent = true })
 
 -- Experiments
 -- Bootstrap lazy.nvim for plugin management
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local fzf
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
 end
@@ -17,29 +25,26 @@ require("lazy").setup({
       highlight = { enable = true },
     })
   end },
-  { "neovim/nvim-lspconfig", config = function()
-    local lspconfig = require("lspconfig")
-    local fzf = require("fzf-lua")
-    lspconfig.ts_ls.setup({ filetypes = { "typescript", "javascript" } })
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(args)
-	vim.keymap.set("n", "gd", fzf.lsp_definitions, { desc = "Find definitions" })
-	vim.keymap.set("n", "gr", fzf.lsp_references, { desc = "Find references" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
-      end,
-    })
-  end },
+
   { "ibhagwan/fzf-lua", config = function()
-    local fzf = require("fzf-lua")
+    fzf = require("fzf-lua")
     vim.keymap.set("n", "<leader>b", fzf.buffers, { desc = "Find buffers" })
     vim.keymap.set("n", "<leader>B", fzf.builtin, { desc = "Builtins" })
     vim.keymap.set("n", "<leader>f", function()
-    	fzf.files({ cwd_prompt = true })
+      fzf.files({ cwd_prompt = true })
     end, { desc = "Find files" })
-    vim.keymap.set("n", "<leader>g", fzf.grep_curbuf, { desc = "Search in file" })
+    vim.keymap.set("n", "<leader>g", fzf.grep_curbuf, { desc = "Search in buffer" })
     vim.keymap.set("n", "<leader>G", fzf.live_grep, { desc = "Search in project" })
-    vim.keymap.set("n", "<leader>r", fzf.registers, { desc = "select registers" })
+    vim.keymap.set("n", "<leader>\"", fzf.registers, { desc = "select registers" })
+    vim.keymap.set("n", "<leader>d", fzf.diagnostics_document, { desc = "buffer diagnostics" })
+    vim.keymap.set("n", "<leader>c", fzf.changes, { desc = "changes" })
+    vim.keymap.set("n", "<leader>r", fzf.lsp_references, { desc = "references" })
+    vim.keymap.set("n", "<leader>s", fzf.lsp_document_symbols, { desc = "buffer symbols" })
+    vim.keymap.set("n", "<leader>d", fzf.lsp_definitions)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    vim.keymap.set("n", "<leader>vs", fzf.git_status)
   end },
+  { "rebelot/kanagawa.nvim" },
 })
 
 -- end experiments
@@ -51,8 +56,114 @@ vim.opt.termguicolors = true
 vim.opt.ignorecase = true
 
 -- themes
-vim.cmd("colorscheme retrobox")
+vim.cmd("colorscheme kanagawa-wave")
 
 -- keymaps
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+-- indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- format on save 
+vim.api.nvim_create_augroup("GoFmt", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.go",
+  callback = function()
+    vim.cmd("silent! !go fmt %")
+  end,
+  group = "GoFmt",
+})
+
+-- remap "+ to leader+
+vim.keymap.set('n', '<leader>y"', 'vi"+y', { noremap = true, silent = true })
+
+
+-- LSP stuff
+-- Start LSPs automatically when relevant filetypes open
+
+-- Go LSP
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.lsp.start({
+      name = "gopls",
+      cmd = { "gopls" },
+      root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
+    })
+  end,
+})
+
+-- TS/JS LSP
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "typescript", "javascript", "json" },
+  callback = function()
+    vim.lsp.start({
+      name = "typescript-language-server",
+      cmd = { "typescript-language-server", "--stdio" },
+      root_dir = vim.fs.root(0, { "package.json", "tsconfig.json", ".git" }),
+    })
+  end,
+})
+
+-- Vimscript LSP
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "vim",
+  callback = function()
+    vim.lsp.start({
+      name = "vimls",
+      cmd = { "vim-language-server", "--stdio" },
+      root_dir = vim.fs.root(0, { ".git" }),
+    })
+  end,
+})
+
+-- Lua LSP
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.lsp.start({
+      name = "lua_ls",
+      cmd = { "lua-language-server" },
+      root_dir = vim.fs.root(0, { ".git", ".luarc.json", ".luarc.jsonc" }),
+      settings = {
+        Lua = {
+          runtime = { version = "LuaJIT" },
+          diagnostics = { globals = { "vim" } },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = { enable = false },
+        },
+      },
+    })
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(args)
+--     local buf = args.buf
+--     local opts = { buffer = buf }
+--   end,
+-- })
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.keymap.set("i", "<C-Space>", "<C-x><C-o>", { desc = "Trigger completion" })
+
+-- End LSP stuff
+
+
+
+-- Open FzfLua automatically if Neovim starts without files
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      require("fzf-lua").live_grep()
+    elseif vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      vim.cmd("cd " .. vim.fn.argv(0))
+      require("fzf-lua").files()
+    end
+  end,
+})
